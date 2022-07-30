6bcc3b1d912c9f9e37953ce60d0dd9999c79f4e1
Gilney Guerra <108212465+gilneyguerra@users.noreply.github.com>
2022-07-30 01:33:35 -0300

    Create alunos_em_array.js

---
+++ alunos_em_array.js
@@ -0,0 +1,27 @@
+var alunos = [
+[6, 7, 8, 6],
+[9, 5, 6, 7],
+[8, 4, 6, 7]
+
+]
+var nota = 0;
+for(var i = 0; i < alunos.length; i++) {
+
+nota = 0
+aluno = alunos[i]
+console.log("Aluno: " + aluno);
+
+for(c = 0; c < aluno.length; c++){
+nota += aluno[c];
+}
+
+media = nota / 4;
+
+if(media >= 6) {
+resultado = "aprovado"
+} else {
+resultado = "reprovado";
+}
+
+console.log("Media: " + media + " - " + resultado);
+}
