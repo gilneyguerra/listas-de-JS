2c88e40ecf0fa57589a4e1a5f2ee0c2ecc3a36ba
Gilney Guerra <108212465+gilneyguerra@users.noreply.github.com>
2022-07-30 02:21:29 -0300

    Update funcoes.js

--- funcoes.js
+++ funcoes.js
@@ -1,11 +1,22 @@
-function calculoMedia ( notas ) {
+function calcularMedia ( notas ) {
 
-var notas = 0;
+let soma = 0;
 for (c = 0; c < notas.length; c++) {
-notas += notas[c];
+soma += notas[c];
 }
 
-media = notas / notas.length;
+media = soma / notas.length;
 return media;
 
 }
+
+function aprovacao ( media ) {
+
+let condicao = media >= 7 ? "aprovado" : "reprovado";
+
+return condicao;
+
+}
+
+console.log ( "Média: " + calcularMedia([8, 8, 9, 10, 5]))
+console.log ( aprovacao( calcularMedia([8, 8, 9, 10, 5])))
